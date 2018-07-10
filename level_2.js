5218d494c24eddf79cdb993fc2dc175d537923eb
M Haidar Hanif <git@mhaidarhanif.com>
2018-07-10 13:22:23 +0700

    Hotfix duplicated variable

--- level_2.js
+++ level_2.js
@@ -1,17 +1,15 @@
 // LEVEL 2
 console.log("LEVEL 2");
 
-let numberA = 100
-let numberB = 200
-let numberC = -300
-let numberD = 1234567890123456789101021021
+let numberI = -300
+let numberJ = 1234567890123456789101021021
 
 let firstName = "Haidar"
 let lastName = "Hanif"
 
 let resultA = "M " + firstName + " " + lastName + "!"
 let resultB = `M ${firstName} ${lastName}?`
-let resultC = numberC + numberD
+let resultC = numberI + numberJ
 
 console.log(resultA);
 console.log(resultB);
