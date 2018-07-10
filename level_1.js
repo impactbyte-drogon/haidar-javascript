28e36baf1505d39fa6921cadcb14850ee9c2cfdd
M Haidar Hanif <git@mhaidarhanif.com>
2018-07-10 13:21:23 +0700

    Standardize variable naming

--- level_1.js
+++ level_1.js
@@ -1,12 +1,12 @@
 // LEVEL 1
 console.log("LEVEL 1");
 
-let numberA = 100
-let numberB = 200
-let numberC = 300
+let numberD = 100
+let numberE = 200
+let numberF = 300
 
-let numberD = numberA + numberB + numberC
-let numberE = numberA * numberB - numberC
+let numberG = numberD + numberE + numberF
+let numberH = numberD * numberE - numberF
 
-console.log("numberD:", numberD);
-console.log("numberE:", numberE);
+console.log("numberD:", numberG);
+console.log("numberE:", numberH);
--- level_2.js
+++ level_2.js
@@ -6,11 +6,11 @@ let numberB = 200
 let numberC = -300
 let numberD = 1234567890123456789101021021
 
-let first_name = "Haidar"
-let last_name = "Hanif"
+let firstName = "Haidar"
+let lastName = "Hanif"
 
-let resultA = "M " + first_name + " " + last_name + "!"
-let resultB = `M ${first_name} ${last_name}?`
+let resultA = "M " + firstName + " " + lastName + "!"
+let resultB = `M ${firstName} ${lastName}?`
 let resultC = numberC + numberD
 
 console.log(resultA);
